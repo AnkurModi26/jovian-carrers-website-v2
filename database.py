731faba0d65a22d5fcd3e0cd5a41be8ace5b07d2
AnkurModi26 <74940431+AnkurModi26@users.noreply.github.com>
2023-05-31 18:05:01 +0000

    Dynamic pages for each job

--- database.py
+++ database.py
@@ -1,15 +1,12 @@
-from sqlalchemy import create_engine,text
+from sqlalchemy import create_engine, text
 import os
 
-db_connecting_string= os.environ["DB_CONNECTION_STRING"]
+db_connecting_string = os.environ["DB_CONNECTION_STRING"]
 
-engine = create_engine(
-  db_connecting_string,
-  connect_args={
-    "ssl":{
-      "ssl_ca": "/etc/ssl/cert.pem"
-      }
-  })
+engine = create_engine(db_connecting_string,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
 
 
 def load_jobs_from_db():
@@ -20,3 +17,16 @@ def load_jobs_from_db():
     for row in result.all():
       jobs.append(dict(zip(column_names, row)))
     return jobs
+
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"),
+                          {"val": id})
+    rows = []
+    for row in result.all():
+      rows.append(row._mapping)
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0])
